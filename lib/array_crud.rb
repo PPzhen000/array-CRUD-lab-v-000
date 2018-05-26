19d76552dad333ee6f9a26434de71f93d9416611
Ariel Zhen <zhenpingping@live.com>
2018-05-26 05:50:10 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
 def create_an_empty_array
-  array = new.Array
+  array = Array.new 
 end
 
 def create_an_array
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  arrar.unshift(element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
